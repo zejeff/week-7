35075b9b38289b9c812961191998e3fc3bc90919
Jeffry Fleischer <jeffryfleischer@gmail.com>
2020-07-16 12:45:05 -0700

    added function for hiding age check button after submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,14 @@
 $(document).ready(function() {
-  
-//  const over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
-  
-//  if (over21) {
-//    $('#alcoholicDrinks').show();
-//    $('#nonAlcoholicDrinks').show();
-//  } else {
-//    $('#nonAlcoholicDrinks').show();
-//  }
-  
-// ToDo: Function to pull from radio buttons and reveal drink menu and hide age check elements
   $('.button').click('submit', function(event) {
     event.preventDefault();
-//    const ageConfirm = "overAge";
     var buttonValue = $('#ageCheckOver').prop('checked');
     if (buttonValue === true) {
       $('#alcoholicDrinks').show();
       $('#nonAlcoholicDrinks').show();
+      $('#ageCheckForm').hide();
     } else {
       $('#nonAlcoholicDrinks').show();
+      $('#ageCheckForm').hide();
     }
   })
 });
