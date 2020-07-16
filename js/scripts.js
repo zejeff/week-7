2bf6b97a93cfdf83fc742adcbc8b6602a49d529e
Jeffry Fleischer <jeffryfleischer@gmail.com>
2020-07-15 17:04:52 -0700

    Add title and sub-title for index.html, add radio buttons for alternate age selection hidden through styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $()
   const over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
-
   if (over21) {
     $('#alcoholicDrinks').show();
     $('#nonAlcoholicDrinks').show();
   } else {
     $('#nonAlcoholicDrinks').show();
   }
+  //ToDo: Function to pull from radio buttons and reveal drink menu and hide age check elements
+  $()
 });
