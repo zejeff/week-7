08ad6246c1cf149c56a60f98fd136a79f10def9a
Jeffry Fleischer <jeffryfleischer@gmail.com>
2020-07-22 16:54:52 -0700

    add logic and page elements for fruit/vegetable menu items

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
-  $('.button').click('submit', function(event) {
+  const color = $('input:radio[name=color]:checked').val();
+  const style = $('input:radio[name=style]:checked').val();
+  $('.foodButton').click('submit', function(event) {
+    if (color === green && style === vegetable) {
+      // Show vegetable salad
+      $("#")
+      $("")
+    } else if () {
+      // Show fruits
+    }
+  })
+  $('.drinksButton').click('submit', function(event) {
     event.preventDefault();
     var buttonValue = $('#ageCheckOver').prop('checked');
     if (buttonValue === true) {
       $('#alcoholicDrinks').show();
       $('#nonAlcoholicDrinks').show();
-      $('#ageCheckForm').hide();
+//      $('#ageCheckForm').hide();
+      $('.redFrog').hide();
     } else {
       $('#nonAlcoholicDrinks').show();
       $('#ageCheckForm').hide();
