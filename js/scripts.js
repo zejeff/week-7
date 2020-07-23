cc14017f857208ef448676d0494381ff39eadff5
Jeffry <jeffryfleischer@gmail.com>
2020-07-22 20:46:59 -0700

    Revert "add logic and page elements for fruit/vegetable menu items"
    This reverts commit 08ad6246c1cf149c56a60f98fd136a79f10def9a.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,11 @@
 $(document).ready(function() {
-  const color = $('input:radio[name=color]:checked').val();
-  const style = $('input:radio[name=style]:checked').val();
-  $('.foodButton').click('submit', function(event) {
-    if (color === green && style === vegetable) {
-      // Show vegetable salad
-      $("#")
-      $("")
-    } else if () {
-      // Show fruits
-    }
-  })
-  $('.drinksButton').click('submit', function(event) {
+  $('.button').click('submit', function(event) {
     event.preventDefault();
     var buttonValue = $('#ageCheckOver').prop('checked');
     if (buttonValue === true) {
       $('#alcoholicDrinks').show();
       $('#nonAlcoholicDrinks').show();
-//      $('#ageCheckForm').hide();
-      $('.redFrog').hide();
+      $('#ageCheckForm').hide();
     } else {
       $('#nonAlcoholicDrinks').show();
       $('#ageCheckForm').hide();
