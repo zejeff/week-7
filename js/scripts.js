806d0648ac8e3b5b9ce72f0e562feb835ea0d75d
Jeffry <jeffryfleischer@gmail.com>
2020-07-22 22:15:37 -0700

    add food menu radio options for four-way selection

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,19 @@ $(document).ready(function() {
       $('#ageCheckForm').hide();
     }
   })
+  $('.foodButton').click('submit', function(event) {
+    // evaluate food style choices with types - four options
+    event.preventDefault();
+    var foodStyle = $('input:radio[name=style]:checked').val();
+    var foodType = $('input:radio[name=type]:checked').val();
+    if (foodType === "F" && foodStyle === "M") {
+      alert("You choose Fruit Medley");
+    } else if (foodType === "F" && foodStyle === "S") {
+      alert("You choose Fruit Salad");
+    } else if (foodType === "V" && foodStyle === "M") {
+      alert("You choose Vegetable Medley");
+    } else if (foodType === "V" && foodStyle === "S") {
+      alert("You choose Salad");
+    }
+  })
 });
